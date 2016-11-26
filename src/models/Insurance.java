50ceb0574c545f44d7ec7c981415e8464a1355ab
Wing Chau <Wing@Wings-MacBook-Pro.local>
2016-11-26 16:14:54 +0000

    edit insurance properties

--- src/models/Insurance.java
+++ src/models/Insurance.java
@@ -17,11 +17,50 @@ public class Insurance {
     private char insuranceNumber;
     private Date startDate, endDate;
 
+    private Car car;
+
     public Insurance(String _company, char _insuranceNumber, Date _startDate, 
-            Date _endDate) {
+            Date _endDate, Car _car) {
         company = _company;
         insuranceNumber = _insuranceNumber;
         startDate = _startDate;
         endDate = _endDate;
+        car = _car;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public char getInsuranceNumber() {
+        return insuranceNumber;
+    }
+
+    public void setInsuranceNumber(char insuranceNumber) {
+        this.insuranceNumber = insuranceNumber;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Car getCar() {
+        return car;
     }
 }
