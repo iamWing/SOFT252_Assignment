7dfab3192002b3475bebdaf0ffa4f17a5e66d85b
Wing Chau <Wing@Wings-MacBook-Pro.local>
2016-11-26 17:59:33 +0000

    implement damage report

---
+++ src/models/DamageRecord.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Wing
+ */
+public class DamageRecord {
+    
+    private Date damageDate;
+    private Staff damagedBy;
+    private String description;
+
+    public DamageRecord(Date _damageDate, Staff _damagedBy, String _desc) {
+        damageDate  = _damageDate;
+        damagedBy = _damagedBy;
+        description = _desc;
+    }
+
+    public Date getDamageDate() {
+        return damageDate;
+    }
+
+    public void setDamageDate(Date damageDate) {
+        this.damageDate = damageDate;
+    }
+
+    public Staff getDamagedBy() {
+        return damagedBy;
+    }
+
+    public void setDamagedBy(Staff damagedBy) {
+        this.damagedBy = damagedBy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
