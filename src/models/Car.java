f116f30c77e9bc76029466b3ebd8dbbf4fcd5979
Wing Chau <Wing@Wings-MacBook-Pro.local>
2016-12-02 17:29:03 +0000

    mark car id as final

--- src/models/Car.java
+++ src/models/Car.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Car {
 
-    private char carId;
+    private final char CARID;
     private String brand;
     private String model;
     private int seats;
@@ -19,20 +19,20 @@ public class Car {
     private boolean damaged = false;
     private boolean enabled = true;
 
-    public Car(char _carId) {
-        carId = _carId;
+    public Car(char _CARID) {
+        CARID = _CARID;
     }
 
-    public Car(char _carId, String _brand, String _model, int _seats) {
-        carId = _carId;
+    public Car(char _CARID, String _brand, String _model, int _seats) {
+        CARID = _CARID;
         brand = _brand;
         model = _model;
         seats = _seats;
     }
 
     // --- Getters & Setters --- //
-    public char getCarId() {
-        return carId;
+    public char getCARID() {
+        return CARID;
     }
 
     public String getBrand() {
