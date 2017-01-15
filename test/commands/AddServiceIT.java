34787f39b014df0bf23da575398f48d9ec828a68
Wing Chau <ccw.wingc@gmail.com>
2017-01-15 22:41:25 +0000

    fix add service command test

--- test/commands/AddServiceIT.java
+++ test/commands/AddServiceIT.java
@@ -11,6 +11,9 @@ import java.util.Calendar;
 import java.util.Date;
 import models.Car;
 import models.CarParks;
+import models.Service;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
@@ -21,7 +24,7 @@ import org.junit.Test;
  * @author Wing
  */
 public class AddServiceIT {
-    
+
     private String vehicleId = "CAR001";
     private String brand = "Honda";
     private String model = "GT86";
@@ -42,7 +45,7 @@ public class AddServiceIT {
         System.out.println("Testing AddService command");
 
         inDate = new Date();
-        
+
         Calendar cal = Calendar.getInstance();
         cal.setTime(inDate);
         cal.add(Calendar.DATE, 1);
@@ -59,24 +62,31 @@ public class AddServiceIT {
         boolean result;
 
         try {
+            // execute
             result = addService.executeCommand();
             assertTrue(result);
-        } catch(Exception ex) {
-            System.out.println(ex.getMessage());
-            fail("Exception caught");
-        }
-    }
+            if (result) {
+                Service service = vehicle.getServiceRecords().get(0);
+                assertEquals(inDate, service.getInDate());
+                assertEquals(outDate, service.getOutDate());
+                assertEquals(serviceDesc, service.getDescription());
+            }
 
-    @Test
-    public void undo() {
-        System.out.println("Undo");
-
-        boolean result;
-
-        try {
+            // undo
             result = addService.undoCommand();
             assertTrue(result);
-        } catch(Exception ex) {
+            if (result) {
+                boolean searched = false;
+                for (Service service : vehicle.getServiceRecords()) {
+                    if ((service.getInDate() == inDate)
+                            && (service.getOutDate() == outDate)
+                            && (service.getDescription().equals(serviceDesc))) {
+                        searched = true;
+                    }
+                }
+                assertFalse(searched);
+            }
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
             fail("Exception caught");
         }
