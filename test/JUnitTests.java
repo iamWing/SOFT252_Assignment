93cc8b53d0dbe4a854e72b1c2e35f6a160155a45
Mike <notmike@notmike.co.uk>
2017-01-15 00:17:10 +0000

    JavaDoc

--- test/JUnitTests.java
+++ test/JUnitTests.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
+ * "Master" JUnit test. Executes other tests.
  *
- * @author Admin
+ * @author NotMike
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
